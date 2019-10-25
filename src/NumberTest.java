0b17ecca573c32f1ae4d3603cd3780769a4370f9
Daria Hauer <dariahauer2@gmail.com>
2019-10-25 14:11:57 +0200

    commit 2 eng

--- src/NumberTest.java
+++ src/NumberTest.java
@@ -5,7 +5,7 @@ public class NumberTest {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbę całkowitą");
+        System.out.println("Please enter the number");
         int number = scanner.nextInt();
 
         System.out.println("Sum of this number is: " + Numbers.numbersSumCalculator(number));
